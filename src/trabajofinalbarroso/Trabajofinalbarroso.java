f7deb78613406e67bccc09e5a8218b276cb7a211
Marcos <monjedeoro@hotmail.com>
2019-07-28 13:11:14 -0600

    Segundo commit

--- src/trabajofinalbarroso/Trabajofinalbarroso.java
+++ src/trabajofinalbarroso/Trabajofinalbarroso.java
@@ -20,24 +20,7 @@ public class Trabajofinalbarroso {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-<<<<<<< HEAD
-        
-        
-        
-        System.out.println("cambio hecho por agustin");
-        System.out.println("segundo cambio agustin");
-        System.out.println("cambiazote Con S");
-        
-        System.out.println("super cambio agustin");
-=======
-        System.out.println("Hola amigos");
-        System.out.println("soy el marcos");
-<<<<<<< HEAD
-        System.out.println("tercer cambio");
-        System.out.println("cuarto cambio krnal");
-=======
->>>>>>> c7d17e3e2a4858d7de6c9989b1f925cf373ee5f0
->>>>>>> f7c6b7b29cc4a4c4df8a0965c26e0da1f1bf38fb
+
         
         
     }
